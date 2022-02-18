aa1201193224908ff4e88c30e691590a6f8aaaa5
vitoriakarpovas <64480334+vitoriakarpovas@users.noreply.github.com>
2022-02-18 08:39:45 -0300

    Create ex2.cpp

---
+++ lab02.cpp/ex2.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+char media(float N1, float N2, float N3, float ME){
+  
+  char nota;
+  int MA;
+
+  MA = (N1 + 2*N2 + 3*N3 + ME)/7;
+
+  if (MA >= 9){
+    nota = 'A';
+  }
+  else if (MA >= 7.5 && MA < 9){
+    nota = 'B';
+  }
+  else if (MA >= 6 && MA < 7.5){
+    nota = 'C';
+  }
+  else if (MA >= 4 && MA < 6){
+    nota = 'D';
+  }
+  else{
+    nota = 'E';
+  }
+  return nota;
+}
+
+int main() 
+{
+  float n1, n2, n3, me;
+  cout << "Por favor, insira a suas notas para que possamos fazer a média: " << endl;
+  cout << "nota N1: ";
+  cin >> n1;
+  cout << "nota N2: ";
+  cin >> n2;
+  cout << "nota N3: ";
+  cin >> n3;
+  cout << "nota ME: ";
+  cin >> me;
+  
+  char notafinal;
+  notafinal = media(n1, n2, n3, me);
+
+  cout << "\nA media final do aluno foi " << notafinal << endl;
+}
