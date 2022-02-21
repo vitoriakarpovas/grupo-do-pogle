eefba678b30f989661d5de4fbac25153390a8a3b
vitoriakarpovas <64480334+vitoriakarpovas@users.noreply.github.com>
2022-02-21 12:19:45 -0300

    Create ex8.cpp

---
+++ lab02.cpp/ex8.cpp
@@ -0,0 +1,51 @@
+// Crie uma função capaz de criar a transposta de uma matriz.
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+
+int main() {
+
+  int LIN = 3;
+  int COL = 3;
+  
+  int matriz[LIN][COL];
+  
+  matriz[0][0] = 1;
+  matriz[0][1] = 2;
+  matriz[0][2] = 3;
+  matriz[1][0] = 4;
+  matriz[1][1] = 5;
+  matriz[1][2] = 6;
+  matriz[2][0] = 7;
+  matriz[2][1] = 8;
+  matriz[2][2] = 9;
+
+  cout << "\nmatriz:" << endl;
+
+  for(int i = 0; i < LIN; i++){
+    for(int k = 0; k < COL; k++){
+      cout << matriz[i][k] << " ";
+    }
+    cout << endl;
+  }
+
+  int matrizTRANS[COL][LIN];
+
+  srand(time(NULL));
+
+  for(int i = 0; i < LIN; i++){
+    for(int k = 0; k < COL; k++){
+      matrizTRANS[k][i] = matriz[i][k];
+    }
+  }
+
+  cout << "matriz transposta:" << endl;
+  for(int i = 0; i < COL; i++){
+    for(int k = 0; k < LIN; k++){
+      cout << matrizTRANS[i][k] << " ";
+    }
+    cout << endl;
+  }
+}
