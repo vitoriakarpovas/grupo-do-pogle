1ca59ec454cef6961ec68a004d533e2bffcce3e2
vitoriakarpovas <64480334+vitoriakarpovas@users.noreply.github.com>
2022-02-21 16:02:07 -0300

    Update ex02.cpp

--- lab02.cpp/ex02.cpp
+++ lab02.cpp/ex02.cpp
@@ -1,3 +1,12 @@
+// Escreva um programa que leia 3 notas de um aluno e a média das notas dos exercícios realizados por ele. Calcular a média de aproveitamento, usando a fórmula: MA = (N1 + N2*2 + N3*3 + ME)/7. A partir da média, informar o conceito de acordo com a tabela:
+/*maior ou igual a 9	A
+maior ou igual a 7.5 e menor que 9	B
+maior ou igual a 6 e menor que 7.5	C
+maior ou igual a 4 e menor que 6	D
+menor que 4	E*/
+
+
+
 #include <iostream>
 #include <stdio.h>
 using namespace std;
