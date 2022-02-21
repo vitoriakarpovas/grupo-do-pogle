f049008ac492c9ff313ac34264b32a4685a84af1
vitoriakarpovas <64480334+vitoriakarpovas@users.noreply.github.com>
2022-02-21 16:01:24 -0300

    Update ex01.cpp

--- lab02.cpp/ex01.cpp
+++ lab02.cpp/ex01.cpp
@@ -1,3 +1,5 @@
+// Crie um programa capaz de ler os dados de uma matriz quadrada de inteiros. Ao final da leitura o programa deverá imprimir o número da linha que contém o menor dentre todos os números lidos.
+
 #include <iostream>
 #include <stdio.h>
 using namespace std;
