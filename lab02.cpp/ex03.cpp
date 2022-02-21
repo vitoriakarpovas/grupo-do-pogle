0deb4b82af9733d8e959459a769f8a152ef3dfa4
vitoriakarpovas <64480334+vitoriakarpovas@users.noreply.github.com>
2022-02-21 16:02:47 -0300

    Update ex03.cpp

--- lab02.cpp/ex03.cpp
+++ lab02.cpp/ex03.cpp
@@ -1,3 +1,10 @@
+// Faça o programa que apresenta a seguinte saída, perguntando ao usuário o número máximo (no exemplo, 9). Este número deve ser sempre ímpar.
+/*1 2 3 4 5 6 7 8 9
+   2 3 4 5 6 7 8
+      3 4 5 6 7
+         4 5 6
+            5*/
+
 #include <iostream>
 #include <stdio.h>
 using namespace std;
